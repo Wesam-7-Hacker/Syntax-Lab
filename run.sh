69f8b6ce6dbc5984c715cf780ef6ef2bb7ecd41d
𝙒𝙚𝙨𝙖𝙢 𝙃𝙖𝙘𝙠𝙚𝙧 𝙤𝙛 𝙄𝙨𝙡𝙖𝙢 <pqcuongg@gmail.com>
2026-06-28 07:04:34 +0300

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,47 @@
+clear
+figlet "WESAM HACKER"
+echo "[Contact]>: Wesam Hacker of Islam"
+sleep 2
+echo "[i]> Wait please..."
+sleep 2
+echo "[i]> Status check: [X]"
+sleep 3
+clear
+figlet WESAM HUB
+echo "[Contact]>: Wesam Hacker of Islam"
+echo "[i]> Wait please..."
+sleep 2
+echo "[i]> Status: [Ok]"
+sleep 2
+clear
+figlet WESAM HACKER
+echo "[Contact]>: Wesam Hacker of Islam"
+echo "[Fixing...]"
+sleep 1
+echo "[Dont close terminal!]"
+sleep 2
+sudo apt-get install python3-tk -y
+python3 -m venv venv
+source venv/bin/activate
+pip install customtkinter requests beautifulsoup4 urllib3 dnspython websocket-client PyJWT cryptography --break-system-packages
+sleep 2
+clear
+sleep 2
+figlet Fixed
+sleep 2
+echo "~\_Wait Tool starting._/~"
+sleep 3
+clear
+figlet "WESAM HACKER"
+echo "[Version]>: Monster Edition Premium Version 1.0"
+echo "[Contact]>: Wesam Hacker of Islam"
+echo "[i]> Dont close terminal!"
+sleep 2
+
+# تشغيل ملف الأداة الرئيسي (تأكد من كتابة اسم الملف هنا كما هو مجود عندك)
+python3 thor-suite.py
+
+clear
+sleep 2
+figlet "BYE BYE ;)"
+echo "[Contact]>: Wesam Hacker of Islam"
